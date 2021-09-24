d305d8515c2738a376680686bde90ee80050e737
wuhuijie <wuhuijie@yiche.com>
2021-09-24 17:33:58 +0800

    提取统计组件.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,52 +1,46 @@
-const Header = ({ course }) => {
-  return (
-    <h1>{course}</h1>
-  );
-}
+import React, { useState } from 'react';
+const Statistics = ({ good, neutral, bad }) => {
+  if (!good && !neutral && !bad) {
+    return <div>No feedback given</div>
+  }
 
-const Part = ({ part, exercises }) => {
-  return (
-    <p>
-      {part} {exercises}
-    </p>
-  );
-}
-const Content = ({ parts }) => {
+  const average = () => {
+    let result = 0;
+    result = (good - bad) / (good + neutral + bad);
+    return result;
+  }
+  const positive = () => {
+    let result = good / (good + neutral + bad);
+    return result * 100 + '%';
+  }
+
   return (
     <div>
-      {parts.map(item => <Part part={item.name} exercises={item.exercises} />)}
+      <div> good {good} </div>
+      <div> neutral {neutral} </div>
+      <div> bad {bad} </div>
+      <div> all {good + neutral + bad} </div>
+      <div> average {average()} </div>
+      <div> positive {positive()} </div>
     </div>
   );
 }
-const Total = ({ parts }) => {
-  return (
-    <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
-  );
-}
-
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [{
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }]
-  };
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={() => setGood(pre => pre + 1)}>good</button>
+        <button onClick={() => setNeutral(pre => pre + 1)}>neutral</button>
+        <button onClick={() => setBad(pre => pre + 1)}>bad</button>
+      </div >
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div >
   );
 }
 
