466ebcb1b1b028ec90ef64b95f49b597bb27a212
wuhuijie <wuhuijie@yiche.com>
2021-09-28 09:25:05 +0800

    feat(part2c-2.13): 多个国家时，增加点击展示详情。

--- part2/courseinfo/src/Country.jsx
+++ part2/courseinfo/src/Country.jsx
@@ -1,13 +1,31 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
+const CountryDetail = ({ name, capital, population, languages }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <h4>capital {capital}</h4>
+      <h4>population {population}</h4>
+      <div>
+        <h3>language</h3>
+        <ul>
+          {languages.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
 const Country = () => {
   const [keyword, setKeyword] = useState('');
   const [tooLong, setTooLong] = useState(0);
   const [showCountries, setShowCountries] = useState([]);
+  const [detailStatus, setDetailStatus] = useState(new Array(10).fill(false));
 
   useEffect(() => {
     axios.get(`https://restcountries.com/v3/name/${keyword}`).then(result => {
+      // axios.get(`https://restcountries.com/v3/all`).then(result => {
       console.log('result: ', result);
       if (result.status === 200) {
         setTooLong(result.data.length);
@@ -16,13 +34,25 @@ const Country = () => {
     })
   }, [keyword]);
 
+  const handleShowCountryDetail = (index) => {
+    detailStatus[index] = !detailStatus[index];
+    setDetailStatus([...detailStatus]);
+  }
+
   const handleShowCountry = () => {
     if (showCountries.length > 1) {
       return (
         <ul>
           {showCountries.map((country, index) => {
             return (
-              <li key={index}>{country.name.common}</li>
+              <>
+                <li key={index}>{country.name.common}</li>
+                <button onClick={() => handleShowCountryDetail(index)}>show</button>
+                {detailStatus[index] ?
+                  <CountryDetail name={country.name.common} capital={country.capital[0]}
+                    population={country.population} languages={Object.values(country.languages)} />
+                  : ''}
+              </>
             );
           })}
         </ul>
@@ -33,17 +63,19 @@ const Country = () => {
         return null;
       }
       return (
-        <div>
-          <h2>{country.name.common}</h2>
-          <h4>capital {country.capital[0]}</h4>
-          <h4>population {country.area}</h4>
-          <div>
-            <h3>language</h3>
-            <ul>
-              {Object.values(country.languages).map((item, index) => <li key={index}>{item}</li>)}
-            </ul>
-          </div>
-        </div>
+        <CountryDetail name={country.name.common} capital={country.capital[0]}
+          population={country.population} languages={Object.values(country.languages)} />
+        // <div>
+        //   <h2>{country.name.common}</h2>
+        //   <h4>capital {country.capital[0]}</h4>
+        //   <h4>population {country.area}</h4>
+        //   <div>
+        //     <h3>language</h3>
+        //     <ul>
+        //       {Object.values(country.languages).map((item, index) => <li key={index}>{item}</li>)}
+        //     </ul>
+        //   </div>
+        // </div>
       );
     }
   }
