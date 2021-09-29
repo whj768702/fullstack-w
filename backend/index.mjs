571111c51a71b8227a252805a3045ad52fae637b
wuhuijie <wuhuijie@yiche.com>
2021-09-29 10:57:49 +0800

    feat(part3-3.1): 增加express服务端

---
+++ backend/index.mjs
@@ -0,0 +1,35 @@
+import express from 'express';
+
+const app = express();
+
+const persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+];
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`server runing on port ${PORT}`);
+})
