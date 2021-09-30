fef06b497abb5cf77ee82ab985f69f14b85c41f0
wuhuijie <wuhuijie@yiche.com>
2021-09-30 11:32:06 +0800

    feat(part3b): 后端服务增加可跨域设置.

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan((tokens, req, res) => {
   if (req.method === 'POST') {
