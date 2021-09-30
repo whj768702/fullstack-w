6b31edd261a4f18ed8a4d36433c0cd0e3aa226cf
wuhuijie <wuhuijie@yiche.com>
2021-09-30 09:10:50 +0800

    feat(part3-3.7): 增加morgan中间件，打印请求信息.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
-import express from 'express';
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('tiny'));
 app.use(express.json());
 
 const persons = [
